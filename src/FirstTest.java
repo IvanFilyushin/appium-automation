1994f76e6b4a52dea27c0f8c52b3ff2ad4dd095d
Ivan Filyushin <ivan.filyushin@gmail.com>
2021-01-12 20:30:25 +0300

    Ex2: Создание метода

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -15,6 +20,7 @@ public class FirstTest {
     public void setUp() throws Exception {
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        WebElement element;
 
         capabilities.setCapability("platformName","Android");
         capabilities.setCapability("deviceName","AndroidTestDevice");
@@ -25,6 +31,10 @@ public class FirstTest {
         capabilities.setCapability("app","C:\\TestAutomation\\Appium\\appium-automation\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        element = driver.findElement
+                (By.xpath("//*[@resource-id='org.wikipedia:id/fragment_onboarding_skip_button']"));
+        element.click();
+
     }
 
     @After
@@ -32,8 +42,63 @@ public class FirstTest {
         driver.quit();
     }
 
+    private WebElement waitForElementPresent(By by, String error_message, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+    private WebElement waitForElementPresent(By by, String error_message){
+        return waitForElementPresent(by,error_message,5);
+    }
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+    private WebElement waitForElementAndSendKeys(By by, String value, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
+    }
+    private boolean waitForElementNotPresent(String xpath, String error_message, long timeoutInSeconds){
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_message + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
+        );
+    }
+    private WebElement waitForElementAndClear(By by, String error_message, long timeoutInSeconds){
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.clear();
+        return element;
+    }
+
+    private void assertElementHasText(By by, String text, String error_message){
+        WebElement element = driver.findElement(by);
+        String elements_text = element.getText();
+        Assert.assertTrue(
+                error_message,
+                elements_text.contains(text)
+        );
+    }
+
     @Test
-    public void firstTest(){
-        System.out.println("This is first appium test");
+    public void inputHasTextTest(){
+        String locator = "//*[@resource-id='org.wikipedia:id/search_container']//*[@class='android.widget.TextView']";
+        String text = "Search Wikipedia";
+
+        WebElement element = waitForElementPresent(
+                By.xpath(locator),
+                "Can't find input field",
+                5);
+
+        assertElementHasText(
+                By.xpath(locator),
+                text,
+                "Input field doesn't contain text '" + text + "'\n"
+        );
     }
 }
